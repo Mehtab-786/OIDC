097e34c98e93d5a321d6dbc45f9d0a2b63d8e986
Mehtab Hussain <tradercoder786@gmail.com>
2026-07-13 23:08:14 +0530

    feat: enhance database schema with primary keys for users, clients, and authcode tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,37 @@
+import { boolean } from "drizzle-orm/cockroach-core";
 import { pgTable, uuid, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  id: uuid().defaultRandom(),
+  id: uuid().primaryKey().defaultRandom(),
   username: varchar({ length: 255 }),
   email: varchar({ length: 255 }).notNull().unique(),
   password: varchar("password", { length: 66 }).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const clients = pgTable("clients", {
+  id: uuid().primaryKey().defaultRandom(),
+  applicationName: varchar("application_name", { length: 255 })
+    .notNull()
+    .unique(),
+  applicationUrl: varchar("application_url", { length: 255 })
+    .notNull()
+    .unique(),
+  redirectUri: varchar("redirect_uri", { length: 66 }).notNull(),
+  clientId: varchar("client_id", { length: 70 }).notNull().unique(),
+  clientSecret: varchar("client_secret", { length: 70 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const authcode = pgTable("authcode", {
+  id: uuid().primaryKey().defaultRandom(),
+  code: varchar({length:80}).notNull(),
+  userId: varchar({ length: 255 }).notNull(),
+  clientId: varchar("client_id", { length: 70 }).notNull(),
+  redirectUri: varchar("redirect_uri", { length: 80 }).notNull(),
+  used:boolean().default(false),
+  expiresAt: timestamp("expires_at").notNull(),
+});
+
