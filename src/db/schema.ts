0e8b51b772b9830264841886401d27700744ff6e
Mehtab Hussain <tradercoder786@gmail.com>
2026-07-16 02:41:36 +0530

    feat: update clients and authcode tables with improved field lengths and unique constraints

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -18,20 +18,30 @@ export const clients = pgTable("clients", {
   applicationUrl: varchar("application_url", { length: 255 })
     .notNull()
     .unique(),
-  redirectUri: varchar("redirect_uri", { length: 66 }).notNull(),
-  clientId: varchar("client_id", { length: 70 }).notNull().unique(),
-  clientSecret: varchar("client_secret", { length: 70 }).notNull(),
+  redirectUri: varchar("redirect_uri", { length: 255 }).notNull(),
+  clientId: varchar("client_id", { length: 255 }).notNull().unique(),
+  clientSecret: varchar("client_secret", { length: 255 }).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
 export const authcode = pgTable("authcode", {
   id: uuid().primaryKey().defaultRandom(),
-  code: varchar({length:80}).notNull(),
-  userId: varchar({ length: 255 }).notNull(),
-  clientId: varchar("client_id", { length: 70 }).notNull(),
-  redirectUri: varchar("redirect_uri", { length: 80 }).notNull(),
+  code: varchar({length:80}).notNull().unique(),
+  userId: uuid("user_id").notNull(),
+  clientId: varchar("client_id", { length: 255 }).notNull(),
+  redirectUri: varchar("redirect_uri", { length: 255 }).notNull(),
   used:boolean().default(false),
   expiresAt: timestamp("expires_at").notNull(),
 });
+
+export const refreshtoken = pgTable("refreshtoken", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  token: varchar("token",{length:255}).notNull().unique(),
+  userId: uuid("user_id").notNull(),
+  clientId: varchar("client_id", { length: 255 }).notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
 
