69583d83d2b4540b20561db9cd3afc462ea9feb5
Mehtab Hussain <tradercoder786@gmail.com>
2026-07-16 00:52:21 +0530

    refactor: clean up key import statements and ensure consistent formatting

--- src/utils/cert.ts
+++ src/utils/cert.ts
@@ -1,14 +1,20 @@
 import fs from "node:fs";
 import path from "node:path";
+import { importSPKI, exportJWK, importPKCS8 } from "jose";
 
-// export const PUBLIC_KEY = fs.readFileSync(path.resolve('../../keys/public.pem'));
-// export const PRIVATE_KEY = fs.readFileSync(path.resolve('../../keys/private.pem'));
 
-export const PRIVATE_KEY = fs.readFileSync(
-  path.join(process.cwd(), "keys", "private.pem"),
-  "utf8"
-);
-export const PUBLIC_KEY = fs.readFileSync(
-  path.join(process.cwd(), "keys", "public.pem"),
-  "utf8"
-);
+export const PRIVATE_KEY = fs.readFileSync( path.join(process.cwd(), "keys", "private.pem"), "utf8" );
+
+export const PUBLIC_KEY = fs.readFileSync( path.join(process.cwd(), "keys", "public.pem"), "utf8" );
+
+
+export async function publicKeyShowing() {
+  const key = await importSPKI(PUBLIC_KEY, "RS256");
+  const jwk = await exportJWK(key);
+  return jwk;
+};
+
+export async function privateKeyShowing() {
+  const key = await importPKCS8(PRIVATE_KEY, "RS256");
+  return key;
+};
